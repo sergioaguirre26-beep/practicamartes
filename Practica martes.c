232ec6f9ce09418d74ea246311208672deeaa970
sergioaguirre26-beep <sergio.aguirre26@uabc.edu.mx>
2026-04-14 09:15:03 -0700

    Primer version Practica martes.c
    se añadio el cuerpo del codigo el cual sera una calculadora simple de sumas y restas.

--- Practica martes.c
+++ Practica martes.c
@@ -3,6 +3,24 @@
 
 int main()
 {
+    int opc;
+    
+    puts("***Calculadora de suma y resta***");
+    puts("1. Suma");
+    puts("2. Resta");
+    scanf("%d",&opc);
+    switch(opc)
+    {
+        case 1:
+            
+        break;
+        case 2:
+            
+        break;
+        default:
+            puts("opcion invalida");
+        break;
+    }
     system("pause");
     return 0;
 }
